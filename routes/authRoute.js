e4f68cad7b51848bdb1f8ccd92a291e760aabe74
Adonis stack <69584141+adonistack@users.noreply.github.com>
2024-09-10 13:03:41 +0300

    Add files via upload

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
 const authController = require('../controllers/authController');
 
-const isDevelopment = process.env.NODE_ENV === 'devlopment';
-
-if (isDevelopment) {
-    router.post('/sign-up', authController.createAccount);
-} else{
-    router.post('/sign-up', (req, res)=> {
-        res.status(403).send('Signup is disabled in production mode');
-    });
-}
-router.post('/sign-in', authController.createLongin);
+router.post('/sign-up', authController.createAccount);
+router.post('/sign-in', authController.createLogin);
+router.get('/users', authController.getAllUsers);
+router.get('/user/:id', authController.getUserById);
+router.get('/current-user', authController.getCurrentUser);
+router.put('/update', authController.updateCurrentUser);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
+router.delete('/delete', authController.deleteAccount);
 router.post('/logout', authController.logout);
-router.get('/profile', authController.getCurrentUser);
-router.get('/users', authController.getAllUsers);
-router.get('/verifyToken', authController.verifyToken);
-router.get('/user/:id', authController.getUserById);
-router.patch('/update-user', authController.updateCurrentUser);
-router.delete('/delete-account', authController.deleteAccount);
-
 
 module.exports = router;
